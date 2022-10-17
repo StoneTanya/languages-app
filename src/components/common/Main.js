493dad4a98fc3ee16b0296814fcaae64f14cd162
StoneTanya <kameneva.tania@mail.ru>
2022-10-17 16:00:26 +0300

    first commit

---
+++ src/components/common/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Main() {
+    return (
+        <React.Fragment>
+            <h2>hello it's me</h2>
+        </React.Fragment>
+    );
+}
+
+export default Main
