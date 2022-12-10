2d4a9ad36e93e9a4d54d25d4436d816e1e460f71
Tanya Stone <kameneva.tania@mail.ru>
2022-12-10 19:16:21 +0300

    added Error Page

--- src/components/pages/errorPage/errorPage.js
+++ src/components/pages/errorPage/errorPage.js
@@ -1,16 +1,29 @@
-import { useRouteError } from "react-router-dom";
+import { useNavigate  } from "react-router-dom";
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import styles from './error.module.scss'
+import { StyledEngineProvider } from '@mui/material/styles';
+import { ReactComponent as ErrorImage } from './errorimg.svg';
 
 export default function ErrorPage() {
-    const error = useRouteError();
-    console.error(error);
+    let navigate = useNavigate();
+
+    const goHome = () => {
+        navigate("/");
+    };
 
     return (
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
-        </div>
+        <StyledEngineProvider injectFirst>
+            <>
+            <Box className={styles.container} id="error-page">
+                <Typography color="error" sx={{ fontSize: 'h3.fontSize' }}>Oops!</Typography>
+                <Typography color="white">Sorry, we can't fined this page! </Typography>
+                <Box>             
+                <ErrorImage/>
+                </Box> 
+                <Button className={styles.button} sx={{ mt: 4 }} variant="contained" size="large" onClick={goHome}>Take me Home</Button>
+            </Box>
+            </>
+        </StyledEngineProvider>
     );
 }
